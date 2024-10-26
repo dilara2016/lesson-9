86875f4ed3084ee3c3a2d8429a2e226911172f9b
dilara2016 <m.123.hunain@gmail.com>
2024-10-26 11:26:52 +0200

    sum of virtual numbers

---
+++ sum of virtual numbers.py
@@ -0,0 +1,9 @@
+#input the value of terms
+n = int(input("Enter the value of terms: "))
+
+sum = 0 #initilise
+i = 1 #initilise
+while i<n: #loop will run to 1 to n
+    sum = sum+i
+    i = 1+i
+print("\nSum =",sum)
